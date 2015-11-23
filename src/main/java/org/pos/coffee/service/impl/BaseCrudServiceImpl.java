145a638f1169ec68781fb7901ab14af83237e531
Laurie <laurie.bonete@gmail.com>
2015-11-23 17:19:46 +0800

    latest commit working search remaining task, search and pagination

--- src/main/java/org/pos/coffee/service/impl/BaseCrudServiceImpl.java
+++ src/main/java/org/pos/coffee/service/impl/BaseCrudServiceImpl.java
@@ -1,6 +1,8 @@
 package org.pos.coffee.service.impl;
 
+import org.pos.coffee.annotation.JoinList;
 import org.pos.coffee.bean.BaseEntity;
+import org.pos.coffee.bean.QueryHelper;
 import org.pos.coffee.dao.BaseEntityDao;
 import org.pos.coffee.service.BaseCrudService;
 import org.pos.coffee.utility.NamingUtil;
@@ -8,6 +10,10 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.Transient;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
@@ -63,7 +69,28 @@ public class BaseCrudServiceImpl<T extends BaseEntity> implements BaseCrudServic
         }
     }
 
-    private void preloadEntity(T entity){
+    private void preloadEntity(T entity) throws Exception{
+        String serviceName=null;
+        for(Field field: entityBeanType.getDeclaredFields()) {
+            field.setAccessible(true);
+            if(!Modifier.isVolatile(field.getModifiers()) &&
+                    !Modifier.isStatic(field.getModifiers()) &&
+                    !field.isAnnotationPresent(Transient.class) &&
+                    field.get(entity)!=null &&
+                    field.get(entity)!=""){
+                Method method = entityBeanType.getMethod(NamingUtil.toGetterName(field.getName()));
+                Object value = method.invoke(entity);
+                if(field.isAnnotationPresent(JoinList.class)
+                        && List.class.isAssignableFrom(method.invoke(entity).getClass())){
+                    List list = (List)method.invoke(entity);
+                    value = (T) list.get(0);
+                }
+                if(BaseEntity.class.isAssignableFrom(value.getClass())){
+                    serviceName = NamingUtil.toAttributeName(value.getClass().getName());
+                }
+                this.baseCrudServiceLoader = (BaseCrudService) beanFactory.getBean(serviceName);
+            }
+        }
         /*this.baseCrudServiceLoader = (BaseCrudService<T>) beanFactory.getBean(serviceBean);*/
     }
 }
